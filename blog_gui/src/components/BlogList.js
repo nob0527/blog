52cdc66f587c124013fbfe17b13073f333321c2a
Balog Norbert <norbet0527.gmail.com>
2020-12-14 19:36:55 +0100

    BlogList useEffect

--- blog_gui/src/components/BlogList.js
+++ blog_gui/src/components/BlogList.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import BlogCard from "./BlogCard";
-import {Mock_BlogList} from "../Mock_BlogList";
+
 
 function BlogList(props) {
+    const [posts, set_post] = useState([])
+
+    const fetch_post = () => {
+
+    }
+
+    useEffect(() => {
+        fetch_post()
+    }, [])
 
     return (
         <div>
             {
-                Mock_BlogList.map(data => <BlogCard key={data.id} blog_data={data}/>)
+                posts.map(data => <BlogCard key={data.id} blog_data={data}/>)
             }
         </div>
     );
